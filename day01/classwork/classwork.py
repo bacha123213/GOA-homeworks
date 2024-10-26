e6b859bb9d88b78eae6a3c387ee4394f5fb60cc1
bacha123213 <turashviligabo@gmail.com>
2024-10-27 00:26:39 +0400

    first commit

--- day01/classwork/classwork.py
+++ day01/classwork/classwork.py
@@ -1,7 +1,1 @@
-print("Bachana Tkemaladze")
-age = 15
-print(age)
-name = ("Bachana")
-print(name)
-username = ("Tkemaladze")
-print(username)
+# classwork argvaq
---
+++ day01/homework/homework.py
@@ -0,0 +1,7 @@
+print("Bachana Tkemaladze")
+age = 15
+print(age)
+name = ("Bachana")
+print(name)
+username = ("Tkemaladze")
+print(username)
