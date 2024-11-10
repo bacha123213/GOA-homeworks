179ce1c4e68b691f891d07ad0713936e08ac014e
bacha123213 <turashviligabo@gmail.com>
2024-11-11 01:18:36 +0400

    second commit

---
+++ day08/homework/homework.py
@@ -0,0 +1,13 @@
+num1 = 10
+num2 = 15
+num3 = 5
+num4 = 3
+num5 = 5
+print((num1 + num2) / num3 - num4*num5 + num1**num3 % num2)
+print((num1 + num2) / num3 - num4-num5 * num1*num3 % num2)
+print((num1 * num2) / num3 - num4*num5 - num1+num3 / num2)
+print((num2 + num1) / num3 - num4+num5 + num1**num3 % num2)
+print((num2 + num3) / num1 - num4+num5 + num1**num3 % num2)
+print((num3 + num1) / num3 * num4+num5 + num1**num1 % num2)
+print((num2 + num1) / num3 - num4+num5 - num1*num3 * num2)
+print((num4 + num1) / num3 - num2+num5 + num1**num3 % num2)
